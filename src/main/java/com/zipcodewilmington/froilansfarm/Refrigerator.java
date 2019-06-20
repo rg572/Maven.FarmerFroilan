3e68291b3b412f116e8b0f377d021047648a9c6b
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-20 14:35:42 -0400

    renamed Edible Storage Refrigerator

--- src/main/java/com/zipcodewilmington/froilansfarm/Refrigerator.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Refrigerator.java
@@ -1,9 +1,9 @@
 package com.zipcodewilmington.froilansfarm;
 
-public abstract class EdibleStorage<T extends Edible> extends Storage<T> {
+public  class Refrigerator<T extends Edible> extends Storage<T> {
+
     void cleanUp() {
     }
-    
 
     Integer find(T t) {
         return null;
