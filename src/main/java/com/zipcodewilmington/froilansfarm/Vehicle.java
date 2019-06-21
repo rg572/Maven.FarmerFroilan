7e75965792199143f8f13c386a526ab541aa3bb0
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-21 13:20:43 -0400

    minor change

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicle.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle.java
@@ -1,7 +1,5 @@
 package com.zipcodewilmington.froilansfarm;
 
 public abstract class Vehicle implements NoiseMaker, Rideable{
-    public String makeNoise() {
-        return null;
-    }
+    public abstract String makeNoise();
 }
