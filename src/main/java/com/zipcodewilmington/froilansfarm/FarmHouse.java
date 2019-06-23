6283f071b0906ddbb63cc052dc2a3a91faf4a9c1
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-23 15:16:30 -0400

    farmhouse is now also a garage

--- src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
@@ -12,9 +12,15 @@ public class FarmHouse extends House {
 
     private final static FarmHouse Instance= new FarmHouse();
     private Refrigerator<Edible> fridge;
+    private Tractor tractor;
+    private CropDuster cropDuster;
+    private CombineHarvester combineHarvester;
 
     private FarmHouse() {
         fridge = new Refrigerator<Edible>();
+        tractor = new Tractor();
+        cropDuster = new CropDuster();
+        combineHarvester = new CombineHarvester();
     }
 
     public Refrigerator<Edible> getFridge(){
