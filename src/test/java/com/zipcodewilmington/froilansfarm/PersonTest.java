dab02037559e71ed32b781d58972d0177c3bb19c
andrewdimaio <andrewdimaio777@gmail.com>
2019-06-23 19:41:31 -0400

    Update PersonTest.java

--- src/test/java/com/zipcodewilmington/froilansfarm/PersonTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/PersonTest.java
@@ -6,14 +6,4 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PersonTest {
-
-    @Test
-    public void makeNoise() {
-        Person person = new Person();
-        String actual = person.makeNoise();
-        String expected = null;
-
-        Assert.assertEquals(expected, actual);
-
-    }
 }
