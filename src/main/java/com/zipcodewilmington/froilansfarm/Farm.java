9d239a251736b086fde0b9cfcc4dc2a3724e7f92
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-23 15:01:56 -0400

    filling out farm

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,7 +1,34 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.List;
+
 public class Farm {
+
+    private Field field;
+
+    private FarmHouse house;
+
+    private List<Stable> stables;
+    private List<ChickenCoop> coops;
+
+
     public Farm() {
 
     }
+
+    protected void setUpFarm(){
+        // setup field
+        field = new Field();
+        field.plantRow(new CropRow<CornStalk>());
+        field.plantRow(new CropRow<TomatoPlant>());
+        field.plantRow(new CropRow<TomatoPlant>());
+        field.plantRow(new CropRow<OkraPlant>());
+        field.plantRow(new CropRow<PotatoPlant>());
+        field.plantRow(new CropRow<SoyPlant>());
+
+        
+    }
+
+
+
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
@@ -1,13 +1,15 @@
 package com.zipcodewilmington.froilansfarm;
 
 public  final class FarmHouse  extends House {
-    public static FarmHouse getInstance() {
-        return Instance;
-    }
 
     private final static FarmHouse Instance= new FarmHouse();
+    private Refrigerator<Edible> fridge;
 
     private FarmHouse() {
+    }
+
+    public static FarmHouse getInstance() {
+        return Instance;
     }
 
 
